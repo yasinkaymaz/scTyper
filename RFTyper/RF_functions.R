b6aedf380720353da0db3b3c84742ae25e35fe15
Yasin Kaymaz <yasinkaymaz@github.com>
2018-10-19 13:19:17 -0400

    added improved RF

--- RFTyper/RF_functions.R
+++ RFTyper/RF_functions.R
@@ -182,14 +182,74 @@ prepareDataset <- function(ExpressionData, CellLabels, do.splitTest=FALSE, perce
   
 }#closes the function
 
-CellTyperTrainer <- function(trainingData, run.name){
+CellTyperTrainer <- function(trainingData, run.name, improve=T){
   library(randomForest)
-  modelname <- paste(run.name, "_rf",sep = "")
   #Added: "sampsize=c(table(trainingData$CellType))". Revisit this later to make sure it is working as expected...
-  modelname = randomForest(CellType~., data = trainingData, norm.votes = TRUE, importance=TRUE, proximity = TRUE, ntree=500, sampsize=c(table(trainingData$CellType)))
-  save(modelname, file=paste(run.name,".RF_model.Robj",sep = ""))
-  print(modelname)
-  #varImpPlot(model, sort = T, n.var=20,main="Top 10 - Variable Importance")
+  rf <- randomForest(CellType~., data = trainingData, norm.votes = TRUE, importance=TRUE, proximity = TRUE, ntree=500, sampsize=c(table(trainingData$CellType)))
+  print(rf)
+  
+  if(improve == T){
+    is.nan.data.frame <- function(x)
+      do.call(cbind, lapply(x, is.nan))
+    
+    rfvotes <- as.data.frame(rf$votes)
+    rfvotes$bestvote <- apply(rfvotes, 1, function(x) max(x))
+    rfvotes$label <- apply(rf$votes, 1, function(x)  names(x)[which.max(x)] )
+    rfvotes$inputnames <- rownames(rfvotes)
+    e <- as.data.frame(table(rfvotes$label))
+    Bestscore <- rfvotes %>% as.tibble() %>% summarize(median=median(bestvote)) %>% c()
+    
+    #Defaults
+    filter_p = 0.05
+    bestvote_cutoff = 0.7
+    badinput_ids <- NULL
+    round_n=1
+    badinput_stats <- data.frame()
+    currentscore = Bestscore$median
+    toss_n = dim(rfvotes)[1]
+    
+    #While Median Best votes score overall is larger in the new iteration AND the number of inputs need to be tossed is larger %1 of all input data, then continue.
+    while (Bestscore$median >= currentscore && toss_n > round(0.01*dim(rfvotes)[1]) ){
+      
+      print(paste("Round number ",round_n))
+      print(paste("Current score is", currentscore,". toss_n is", toss_n, ". Fractions is", round(0.01*dim(rfvotes)[1])))
+      
+      for(i in 1:length(e$Var1)){
+        badinputs <- rfvotes %>% as.tibble() %>% filter(., label == e$Var1[i] & bestvote < bestvote_cutoff) %>% arrange(bestvote) %>% top_n(n=round(filter_p*e$Freq[i]), wt=-bestvote) %>% select(inputnames) %>% c()
+        badinputs.m <- rfvotes %>% as.tibble() %>% filter(., label == e$Var1[i] & bestvote < bestvote_cutoff) %>% arrange(bestvote) %>% top_n(n=round(filter_p*e$Freq[i]), wt=-bestvote) %>% summarize(mean=mean(bestvote)) %>% c()
+        badinput_ids <- unique(c(badinput_ids, badinputs$inputnames))
+        classBestscore <- rfvotes %>% as.tibble() %>%  filter(., label == e$Var1[i]) %>% summarize(median=median(bestvote)) %>% c()
+        #print(paste("The number of input dropped for",e$Var1[i],"class is", length(badinputs$inputnames),sep = " "))
+        class_badinput_stats <- data.frame(class=e$Var1[i], classInputSize=e$Freq[i], allBestscoreMedian=Bestscore$median, classBestscoreMedian=classBestscore$median, tossedInput=length(badinputs$inputnames), tossedBestvoteMean=badinputs.m$mean, iteration=round_n)
+        badinput_stats <- rbind(badinput_stats, class_badinput_stats)
+      }
+      
+      badinput_stats[is.nan(badinput_stats)] <- 0
+      toss_n <- badinput_stats %>% as.tibble() %>% filter(., iteration == round_n) %>% summarise(n=sum(tossedInput)) %>% c()
+      toss_n <- toss_n$n
+      
+      print(badinput_stats)
+      
+      #filter input using the bad input list generated in the previous iteration
+      trainingData <- trainingData[which(!rownames(trainingData) %in% badinput_ids ),]
+      
+      #run the RF again with the updated training set
+      rf <- randomForest(CellType~., data = trainingData, norm.votes = TRUE, importance=TRUE, proximity = TRUE, ntree=500, sampsize=c(table(trainingData$CellType)))
+      print(rf)
+      #Check again to see if there is room to improve:
+      rfvotes <- as.data.frame(rf$votes)
+      rfvotes$bestvote <- apply(rfvotes, 1, function(x) max(x))
+      rfvotes$label <- apply(rf$votes, 1, function(x)  names(x)[which.max(x)] )
+      rfvotes$inputnames <- rownames(rfvotes)
+      e <- as.data.frame(table(rfvotes$label))
+      Bestscore <- rfvotes %>% as.tibble() %>% summarize(median=median(bestvote)) %>% c()
+      #update the round
+      round_n = round_n + 1
+    }#closes the while loop
+  
+  }#closes the improve option
+  save(rf, file=paste(run.name,".RF_model.Robj",sep = ""))
+  return(rf)
 }
 
 
--- RFTyper/TestingRF_v5.R
+++ RFTyper/TestingRF_v5.R
@@ -26,3 +26,117 @@ for (d in datas){
   SeuratWrapper(d, exp.data, Normalize = T, Label = d)
   rm(exp.data)
 }
+
+
+merged <- ScaleData(merged,genes.use = hv.genes)
+
+merged <- RunPCA(merged,
+                 pc.genes = hv.genes,
+                 do.print = FALSE)
+
+merged <- FindClusters(merged,
+                       reduction.type = "pca",
+                       dims.use = 1:10,
+                       resolution = 1,
+                       print.output = FALSE,
+                       save.SNN = TRUE,
+                       force.recalc = T)
+merged <- RunTSNE(merged, dims.use = 1:10, do.fast = TRUE,check_duplicates = FALSE)
+
+head(merged@meta.data)
+
+load("rf.with.strata.RF_model.Robj")
+rf <- modelname
+
+merged <- CellTyper(SeuratObject = merged, model = rf)
+PlotPredictions(SeuratObject = merged, model = rf, outputFilename = "merged.CD14-CD19-CD34.predictions")
+
+TSNEPlot(merged, do.label = T)
+
+
+
+#optimize rf model
+
+load("rf.with.strata.RF_model.Robj")
+rf <- modelname
+
+print(rf)
+is.nan.data.frame <- function(x)
+  do.call(cbind, lapply(x, is.nan))
+
+filter_p = 0.05
+bestvote_cutoff = 0.7
+badinput_ids <- NULL
+round_n=1
+badinput_stats <- data.frame()
+
+rfvotes <- as.data.frame(rf$votes)
+rfvotes$bestvote <- apply(rfvotes, 1, function(x) max(x))
+rfvotes$label <- apply(rf$votes, 1, function(x)  names(x)[which.max(x)] )
+rfvotes$inputnames <- rownames(rfvotes)
+e <- as.data.frame(table(rfvotes$label))
+
+Bestscore <- rfvotes %>% as.tibble() %>% summarize(median=median(bestvote)) %>% c()
+# 
+# for(i in 1:length(e$Var1)){
+#   badinputs <- rfvotes %>% as.tibble() %>% filter(., label == e$Var1[i] & bestvote < bestvote_cutoff) %>% arrange(bestvote) %>% top_n(n=round(filter_p*e$Freq[i]), wt=-bestvote) %>% select(inputnames) %>% c()
+#   badinputs.m <- rfvotes %>% as.tibble() %>% filter(., label == e$Var1[i] & bestvote < bestvote_cutoff) %>% arrange(bestvote) %>% top_n(n=round(filter_p*e$Freq[i]), wt=-bestvote) %>% summarize(mean=mean(bestvote)) %>% c()
+#   badinput_ids <- unique(c(badinput_ids, badinputs$inputnames))
+#   classBestscore <- rfvotes %>% as.tibble() %>%  filter(., label == e$Var1[i]) %>% summarize(median=median(bestvote)) %>% c()
+#   print(paste("The number of input dropped for",e$Var1[i],"class is", length(badinputs$inputnames),sep = " "))
+#   class_badinput_stats <- data.frame(class=e$Var1[i], classInputSize=e$Freq[i], allBestscoreMedian=Bestscore$median, classBestscoreMedian=classBestscore$median,  tossedInput=length(badinputs$inputnames), tossedBestvoteMean=badinputs.m$mean, iteration=round_n)
+#   badinput_stats <- rbind(badinput_stats, class_badinput_stats)
+# }
+# 
+# badinput_stats[is.nan(badinput_stats)] <- 0
+# toss_n <- badinput_stats %>% as.tibble() %>% filter(., iteration == round_n) %>% summarise(n=sum(tossedInput)) %>% c()
+# toss_n <- toss_n$n
+
+#Repeat the process until the Median Best score improves
+#currentscore <- badinput_stats %>% as.tibble() %>% filter(., iteration == round_n) %>% select(allBestscoreMedian) %>% c()
+#currentscore <- currentscore$allBestscoreMedian[1]
+
+#Defaults
+
+currentscore = Bestscore$median
+toss_n = dim(rfvotes)[1]
+
+#While Median Best votes score overall is larger in the new iteration AND the number of inputs need to be tossed is larger %1 of all input data, then continue.
+while (Bestscore$median >= currentscore && toss_n > round(0.01*dim(rfvotes)[1]) ){
+
+  print(paste("Round number ",round_n))
+  print(paste("Current score is", currentscore,". toss_n is", toss_n, ". Fractions is", round(0.01*dim(rfvotes)[1])))
+
+  for(i in 1:length(e$Var1)){
+    badinputs <- rfvotes %>% as.tibble() %>% filter(., label == e$Var1[i] & bestvote < bestvote_cutoff) %>% arrange(bestvote) %>% top_n(n=round(filter_p*e$Freq[i]), wt=-bestvote) %>% select(inputnames) %>% c()
+    badinputs.m <- rfvotes %>% as.tibble() %>% filter(., label == e$Var1[i] & bestvote < bestvote_cutoff) %>% arrange(bestvote) %>% top_n(n=round(filter_p*e$Freq[i]), wt=-bestvote) %>% summarize(mean=mean(bestvote)) %>% c()
+    badinput_ids <- unique(c(badinput_ids, badinputs$inputnames))
+    classBestscore <- rfvotes %>% as.tibble() %>%  filter(., label == e$Var1[i]) %>% summarize(median=median(bestvote)) %>% c()
+    print(paste("The number of input dropped for",e$Var1[i],"class is", length(badinputs$inputnames),sep = " "))
+    class_badinput_stats <- data.frame(class=e$Var1[i], classInputSize=e$Freq[i], allBestscoreMedian=Bestscore$median, classBestscoreMedian=classBestscore$median, tossedInput=length(badinputs$inputnames), tossedBestvoteMean=badinputs.m$mean, iteration=round_n)
+    badinput_stats <- rbind(badinput_stats, class_badinput_stats)
+  }
+  
+  badinput_stats[is.nan(badinput_stats)] <- 0
+  toss_n <- badinput_stats %>% as.tibble() %>% filter(., iteration == round_n) %>% summarise(n=sum(tossedInput)) %>% c()
+  toss_n <- toss_n$n
+  
+  print(badinput_stats)
+  
+  #filter input using the bad input list generated in the previous iteration
+  trainingData.postPCA <- trainingData.postPCA[which(!rownames(trainingData.postPCA) %in% badinput_ids ),]
+  
+  #run the RF again with the updated training set
+  rf <- randomForest(CellType~., data = trainingData.postPCA, norm.votes = TRUE, importance=TRUE, proximity = TRUE, ntree=500, sampsize=c(table(trainingData.postPCA$CellType)))
+  print(rf)
+  #Check again to see if there is room to improve:
+  rfvotes <- as.data.frame(rf$votes)
+  rfvotes$bestvote <- apply(rfvotes, 1, function(x) max(x))
+  rfvotes$label <- apply(rf$votes, 1, function(x)  names(x)[which.max(x)] )
+  rfvotes$inputnames <- rownames(rfvotes)
+  e <- as.data.frame(table(rfvotes$label))
+  Bestscore <- rfvotes %>% as.tibble() %>% summarize(median=median(bestvote)) %>% c()
+  #update the round
+  round_n = round_n + 1
+}
+
